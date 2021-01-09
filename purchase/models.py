1f370cf841a2cde30b76c3f32d0d12fd9d7d2790
Zeeshan <zeeshan.ahmed@crownpeak.com>
2021-01-10 00:41:45 +0530

    z - purchase app initial

--- purchase/models.py
+++ purchase/models.py
@@ -1,8 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-class Purchase(models.Model):
-    product_name = models.CharField(max_length=30)
-    cost_price = models.DecimalField(decimal_places=2,max_digits=10)
-    quantity = models.IntegerField()
--- purchase/views.py
+++ purchase/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def purchase_entry(request):
+    return render(request, 'purchase_entry.html')
