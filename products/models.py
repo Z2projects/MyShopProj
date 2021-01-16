341c51f613bbdb32f8378418ae81a2c6a4620707
Zeeshan <zeeshan.ahmed@crownpeak.com>
2021-01-16 18:42:01 +0530

    add new type field

--- products/models.py
+++ products/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=10)
+    ptype = models.CharField(max_length=10)
     rate = models.IntegerField()
     quantity = models.IntegerField()
     cost_price = models.IntegerField()
--- products/views.py
+++ products/views.py
@@ -13,10 +13,11 @@ def product_list(request):
 
 def product_save(request):
     pn = request.POST["product_name"]
+    t = request.POST["product_type"]
     r = request.POST["rate"]
     q = request.POST["quantity"]
     cp = int(r)*int(q)
-    p = Product.objects.create(name=pn, rate=r, quantity=q, cost_price=cp)
+    p = Product.objects.create(name=pn, ptype=t, rate=r, quantity=q, cost_price=cp)
     return HttpResponse("product saved")
 
 def product_delete(request):
@@ -32,7 +33,7 @@ def product_search(request):
 def product_detail(request):
     try:
         res = Product.objects.get(name=request.POST["product_name"])
-        data = {'name': res.name, 'rate': res.rate, 'quantity': res.quantity, 'cost_price': res.cost_price}
+        data = {'name': res.name, 'prod_type': res.ptype, 'rate': res.rate, 'quantity': res.quantity, 'cost_price': res.cost_price}
         return HttpResponse(json.dumps(data))
     except Product.DoesNotExist:
         return HttpResponse("product does not exist")
