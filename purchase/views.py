8f66b83bf5b88e7abd6e114c01f9a29176dd9e01
Zeeshan <zeeshan.ahmed@crownpeak.com>
2021-01-16 14:11:44 +0530

    remove all unwanted files

--- purchase/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def purchase_entry(request):
-    return render(request, 'purchase_entry.html')
